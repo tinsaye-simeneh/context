1a939f832d0c98f7c5ed3bd3ab1212f1620110b2
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-03-03 11:36:04 +0300

    Login style chnaged using react-bootstrap

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,22 +1,36 @@
 import React, { useState, useContext } from "react";
 import { LoginContext } from "../Context/LoginContext";
+import { Container, Form, Button } from "react-bootstrap";
 
 export default function Login() {
-const {username, setUsername, setIsLoggedIn} = useContext(LoginContext)
+  const { username, setUsername, setIsLoggedIn } = useContext(LoginContext);
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input type="password" placeholder="Password" />
-      <button onClick={() => setIsLoggedIn(true)}>
-        Login
-      </button>
-    </div>
+    <Container className="bg-dark my-5 pb-5">
+      <h1 className="text-center text-white py-4">Login</h1>
+      <form className="w-50 mx-auto">
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={() => setIsLoggedIn(true)}
+          className="w-100 mt-5"
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
   );
 }
